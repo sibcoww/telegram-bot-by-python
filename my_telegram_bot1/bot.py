35306c482c3bcc6febb67ece631b14617cd1bcdc
sibcoww <ermek.zhubataev@gmail.com>
2025-06-17 06:28:25 +0500

    Deleted /help command and added /menu instead /start

--- my_telegram_bot1/bot.py
+++ my_telegram_bot1/bot.py
@@ -4,25 +4,19 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("START")
+async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("MENU")
     keyboard = [
-        ["Make a sticker", "Use google drive", "Help"]
+        ["Make a sticker", "Use google drive", "Menu"]
     ]
 
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
     await update.message.reply_text("Hello, I'm your bot!", reply_markup=reply_markup)
 
-async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("HELP")
-    await update.message.reply_text("/start - reset the bot\n" \
-    "/help - write this text\n" \
-    "/makesticker - make sticker by photo", reply_markup = ReplyKeyboardRemove())
-
 async def handler_any(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.photo and context.user_data.get("waiting_for_photo"):
-        await update.message.reply_text("I started to do sticker")
+        await update.message.reply_text("I menued to do sticker")
         context.user_data["waiting_for_photo"] = False
 
         photo = update.message.photo[-1]
@@ -35,7 +29,7 @@ async def handler_any(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif context.user_data.get("waiting_for_photo") and not update.message.photo:
         context.user_data["waiting_for_photo"] = False
         await update.message.reply_text("It's not a photo. I reset your choose")
-        await help(update, context)
+        await menu(update, context)
 
     elif update.message.sticker :
         await update.message.reply_text("Cool sticker bro!")
@@ -44,11 +38,11 @@ async def handler_any(update: Update, context: ContextTypes.DEFAULT_TYPE):
         text = update.message.text.lower() 
         if text == "make a sticker":
             await makesticker(update, context)
-        elif text == "help":
-            await help(update, context)
+        elif text == "menu":
+            await menu(update, context)
         elif text == "use google drive":
+            context.user_data["WorkInGoogleDrive"] = True
             await google_drive(update, context)
-            await context.user_data["WorkInGoogleDrive"] = True
         elif "hi" in text or "hello" in text:
             await update.message.reply_text("Hello my dear user))")
         elif "bye" in text or "bb" in text or "goodbye" in text:
@@ -66,8 +60,7 @@ async def google_drive(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == "__main__":
     token = os.getenv("BOT_TOKEN")
     app = ApplicationBuilder().token(token).build()
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("help", help))
+    app.add_handler(CommandHandler("menu", menu))
 
     app.add_handler(MessageHandler(~filters.COMMAND, handler_any))
     app.add_handler(CommandHandler("makesticker", makesticker))
